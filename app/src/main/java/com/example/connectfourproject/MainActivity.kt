b012a41f41fbeda76e90d65be22b91401c42723d
SilvaCastill0 <sich23ll@student.ju.se>
2024-11-04 00:33:10 +0100

    Layouts

--- app/src/main/java/com/example/connectfourproject/MainActivity.kt
+++ app/src/main/java/com/example/connectfourproject/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.connectfourproject
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.runtime.Composable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton = findViewById<Button>(R.id.myButton)
+        myButton.setOnClickListener {
+            showMessage("Button Clicked!")
+        }
+    }
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
-
-//Test commit
