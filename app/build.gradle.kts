1dfce43f0a20114fb8a5f08ddf58876100a6f0a0
SilvaCastill0 <sich23ll@student.ju.se>
2024-11-04 20:50:17 +0100

    Layout design and new activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
-        compose = true
+        viewBinding = true
     }
 }
 
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/connectfourproject/MainActivity.kt
+++ app/src/main/java/com/example/connectfourproject/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.example.connectfourproject
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SmallFloatingActionButton
-import androidx.compose.runtime.Composable
+import com.example.connectfourproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val myButton = findViewById<Button>(R.id.myButton)
-        myButton.setOnClickListener {
-            showMessage("Button Clicked!")
+        binding.singleplayerBtn.setOnClickListener {
+            createOfflineGame()
         }
     }
-    private fun showMessage(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+    fun createOfflineGame(){
+        startGame()
     }
+
+    fun startGame(){
+        startActivity(Intent(this,GameActivity::class.java))
+    }
+
 }
